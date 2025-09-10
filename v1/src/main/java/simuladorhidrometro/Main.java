cad04a9ede1b391e2f2c4559ed301a861bfe3c10
Pablo Daniel <137708030+MmonkeyBu@users.noreply.github.com>
2025-09-09 23:23:08 -0300

    Update Main.java

--- v1/src/main/java/simuladorhidrometro/Main.java
+++ v1/src/main/java/simuladorhidrometro/Main.java
@@ -4,24 +4,17 @@ package simuladorhidrometro;
 
 public class Main {
     public static void main(String[] args) {
-        // Usamos o SwingUtilities.invokeLater para garantir que a interface gráfica
-        // seja criada e manipulada na thread correta. É a prática mais segura em Swing.
+
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                // 1. CRIAÇÃO: Cria todas as peças do nosso sistema MVC.
+
                 Hidrometro modelo = new Hidrometro();
                 ConfiguracaoView viewConfig = new ConfiguracaoView();
                 HidrometroView viewPrincipal = new HidrometroView();
 
-                // 2. LIGAÇÃO: Cria o Controller, "apresentando" todas as peças a ele.
                 HidrometroController controller = new HidrometroController(modelo, viewConfig, viewPrincipal);
-                
-                // 3. INICIAÇÃO: Manda o controller "ligar os fios" e começar a ouvir os botões.
-                // É ESTE MÉTODO QUE FAZ OS BOTÕES FUNCIONAREM!
-                controller.iniciar();
 
-                // 4. PONTO DE PARTIDA: Exibe APENAS a primeira tela.
-                // O controller se encarregará de fechar esta e abrir a próxima.
+                controller.iniciar();
                 viewConfig.exibir();
             }
         });
